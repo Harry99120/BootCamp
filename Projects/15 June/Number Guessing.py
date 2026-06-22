f220ab91088f32ca9599c7ec3201da954f029441
Harmeet <harmeetbhati99@gmail.com>
2026-06-22 12:27:02 +0530

    22 June

--- Projects/15 June/Number Guessing.py
+++ Projects/15 June/Number Guessing.py
@@ -1,8 +1,24 @@
-import random 
-N = int(input("Guess a number from 0 to 11-->"))
-X = random.randint(0,11)
-if X == N :
-    print("Correct Number")
-else :
-    print("Not Correct")
+import random
 
+n = random.randint(1, 100)
+
+max_tries = 10
+tries = 0
+
+while tries < max_tries:
+    try:
+        a = int(input("Guess a number between 1 and 100: "))
+        tries += 1
+        if a < n:
+            print("Very Low")
+        elif a > n:
+            print("Too high!")
+        else:
+            print("Huraaaaaahhhhhh ! You guessed the number.")
+            break
+    except ValueError:
+        print("Invalid Entry Please enter an integer.")
+
+if tries == max_tries:
+    print("Sorry, you have reached the maximum number of tries.")
+    print("The number was:", n)
